efbc770496d550789a37b9130003826cc4689297
Michał Skwira <michalskwira@yahoo.com>
2018-11-12 09:38:24 +0100

    Task done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var colors = require('colors');
 
-fs.readdir('./tekst.txt', 'utf-8', function(err, data) {
+fs.readdir('./', 'utf-8', function(err, data) {
     console.log(data);
     fs.writeFile('./tek.txt', '\nZapis!', function(err) {
         if (err) throw err;
