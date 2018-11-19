7a273c04c2b82d2a400bb0a221f58c914f111e8d
Michał Skwira <michalskwira@yahoo.com>
2018-11-19 14:40:25 +0100

    task fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var colors = require('colors');
 
 fs.readdir('./', 'utf-8', function(err, data) {
     console.log(data);
-    fs.writeFile('./tek.txt', '\nZapis!', function(err) {
+    fs.writeFile('./tek.txt', data, function(err) {
         if (err) throw err;
     });
 });
